9948c39d568f7aa708b8d712b1cc3bf162c0c84d
KacperLorenc <klorenc@sigma.ug.edu.pl>
2020-12-15 21:45:37 +0100

    adnotacja dla usera

--- src/main/java/pl/olafszewczak/dodohow/entities/User.java
+++ src/main/java/pl/olafszewczak/dodohow/entities/User.java
@@ -5,16 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "User")
+@Entity
 public class User {
 
     @Id
