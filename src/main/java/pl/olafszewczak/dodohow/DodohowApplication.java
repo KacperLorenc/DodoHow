36ee52df0b4b3898aa7374ffb766bd30e129d53c
KacperLorenc <klorenc@sigma.ug.edu.pl>
2020-12-15 20:55:48 +0100

    czysty projekt

---
+++ src/main/java/pl/olafszewczak/dodohow/DodohowApplication.java
@@ -0,0 +1,13 @@
+package pl.olafszewczak.dodohow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodohowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodohowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/olafszewczak/dodohow/DodohowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.olafszewczak.dodohow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodohowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
